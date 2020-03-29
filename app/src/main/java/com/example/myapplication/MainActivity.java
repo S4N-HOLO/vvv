ce0d4b25dd79f41176487698595b54d844eb1ebd
Александр Щербаков <tommynotbad@outlook.com>
2020-03-30 02:07:03 +0300

    added second activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,16 +3,39 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    int i, x = 0;
+    private Button b;
+    private EditText edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hehe();
+    }
+
+    public void  hehe() {
+        b = (Button)findViewById(R.id.button3);
+        edit = (EditText)findViewById(R.id.editText3);
+        b.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (edit.length() != 0)
+                            Toast.makeText(
+                                MainActivity.this,
+                                "уууу" + edit.getText(),
+                                Toast.LENGTH_SHORT
+                            ).show();
+                    }
+                }
+        );
     }
 
 
